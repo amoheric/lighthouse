27eedba046c85bfbf515285b04bc1cdbf208ff52
Adam Raine <ad.st.raine@gmail.com>
2022-07-20 15:57:44 -0700

    units

--- lighthouse-core/test/fraggle-rock/replay/stringify-extension-test.js
+++ lighthouse-core/test/fraggle-rock/replay/stringify-extension-test.js
@@ -20,6 +20,162 @@ const replayFlowJson = readJson(`${LH_ROOT}/lighthouse-core/test/fixtures/fraggl
 const FLOW_JSON_REGEX = /window\.__LIGHTHOUSE_FLOW_JSON__ = (.*);<\/script>/;
 
 describe('LighthouseStringifyExtension', () => {
+  it('handles ending timespan', async () => {
+    /** @type {import('@puppeteer/replay').Schema.UserFlow} */
+    const flowJson = {
+      title: 'Test Flow',
+      steps: [
+        {
+          type: 'setViewport',
+          width: 757,
+          height: 988,
+          deviceScaleFactor: 3,
+          isMobile: true,
+          hasTouch: true,
+          isLandscape: false,
+        },
+        {
+          type: 'navigate',
+          url: 'https://example.com',
+          assertedEvents: [
+            {
+              type: 'navigation',
+              url: 'https://example.com',
+              title: '',
+            },
+          ],
+        },
+        {
+          type: 'click',
+          target: 'main',
+          selectors: [['#button']],
+          offsetY: 13.5625,
+          offsetX: 61,
+        },
+      ],
+    };
+
+    const scriptContents = await stringify(flowJson, {
+      extension: new LighthouseStringifyExtension(),
+    });
+
+    // Trim the output to the relevant stuff
+    const endIndex = scriptContents.indexOf('browser.close');
+    const relevantOutput = scriptContents.substring(0, endIndex);
+
+    expect(relevantOutput).toMatchSnapshot();
+  });
+
+  it('handles ending navigation', async () => {
+    /** @type {import('@puppeteer/replay').Schema.UserFlow} */
+    const flowJson = {
+      title: 'Test Flow',
+      steps: [
+        {
+          type: 'setViewport',
+          width: 757,
+          height: 988,
+          deviceScaleFactor: 3,
+          isMobile: true,
+          hasTouch: true,
+          isLandscape: false,
+        },
+        {
+          type: 'navigate',
+          url: 'https://example.com',
+          assertedEvents: [
+            {
+              type: 'navigation',
+              url: 'https://example.com',
+              title: '',
+            },
+          ],
+        },
+        {
+          type: 'click',
+          target: 'main',
+          selectors: [['#button']],
+          offsetY: 13.5625,
+          offsetX: 61,
+        },
+        {
+          type: 'navigate',
+          url: 'https://example.com/page/',
+          assertedEvents: [
+            {
+              type: 'navigation',
+              url: 'https://example.com/page/',
+              title: '',
+            },
+          ],
+        },
+      ],
+    };
+
+    const scriptContents = await stringify(flowJson, {
+      extension: new LighthouseStringifyExtension(),
+    });
+
+    // Trim the output to the relevant stuff
+    const endIndex = scriptContents.indexOf('browser.close');
+    const relevantOutput = scriptContents.substring(0, endIndex);
+
+    expect(relevantOutput).toMatchSnapshot();
+  });
+
+  it('handles multiple sequential navigations', async () => {
+    /** @type {import('@puppeteer/replay').Schema.UserFlow} */
+    const flowJson = {
+      title: 'Test Flow',
+      steps: [
+        {
+          type: 'setViewport',
+          width: 757,
+          height: 988,
+          deviceScaleFactor: 3,
+          isMobile: true,
+          hasTouch: true,
+          isLandscape: false,
+        },
+        {
+          type: 'navigate',
+          url: 'https://example.com',
+          assertedEvents: [
+            {
+              type: 'navigation',
+              url: 'https://example.com',
+              title: '',
+            },
+          ],
+        },
+        {
+          type: 'click',
+          target: 'main',
+          selectors: [['#link']],
+          offsetY: 13.5625,
+          offsetX: 61,
+          assertedEvents: [
+            {
+              type: 'navigation',
+              url: 'https://example.com/page',
+              title: '',
+            },
+          ],
+        },
+      ],
+    };
+
+    const scriptContents = await stringify(flowJson, {
+      extension: new LighthouseStringifyExtension(),
+    });
+
+    // Trim the output to the relevant stuff
+    const endIndex = scriptContents.indexOf('browser.close');
+    const relevantOutput = scriptContents.substring(0, endIndex);
+
+    expect(relevantOutput).toMatchSnapshot();
+  });
+
   describe('running the output script', function() {
     // eslint-disable-next-line no-invalid-this
     this.timeout(60_000);
